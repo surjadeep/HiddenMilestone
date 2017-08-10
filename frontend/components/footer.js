70867478cd7495981dcec129cd041b49c6d91db1
Surjadeep Banerjee <study.surja@gmail.com>
2017-08-10 11:26:51 +0530

    Update footer.js

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -5,7 +5,7 @@ const { Footer } = Layout;
 export default class SiteFooter extends Component {
 	render() {
 		return (
-			<Footer style={{ width: '100%', backgroundColor:'#000', color:'#eee' }}>
+			<Footer className="SiteFooterCls">
 				 <div>Copyright © 2017. Hidden Milestone | All rights reseved.</div>
 				 <div style={{ float: 'right', marginTop:'-18px', color:'red' }}>Developed & Maintained by Surjadeep</div>
 			 </Footer>
